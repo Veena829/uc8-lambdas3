3b359f9268c23ed4fecb70cdd3e3a7af9280884d
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-23 13:08:35 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,31 +1,42 @@
+# Define the source bucket
 resource "aws_s3_bucket" "source_bucket" {
-  bucket = var.source_bucket_name
-
-  tags = {
-    Name = var.source_bucket_name
-  }
+  bucket = "source-bucket-images1111121"
 }
 
-resource "aws_s3_bucket_versioning" "source_versioning" {
+# Define the destination bucket
+resource "aws_s3_bucket" "destination_bucket" {
+  bucket = "destination-bucket-images11121"
+}
+
+# Add Lambda permission to allow S3 to invoke the Lambda function
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.source_bucket.arn
+}
+
+# Configure S3 bucket notification to invoke Lambda function on object creation
+resource "aws_s3_bucket_notification" "source_bucket_notification" {
   bucket = aws_s3_bucket.source_bucket.id
 
-  versioning_configuration {
-    status = "Enabled"
+  lambda_function {
+    lambda_function_arn = var.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
   }
+
+  depends_on = [
+    aws_lambda_permission.allow_s3
+  ]
 }
 
-resource "aws_s3_bucket" "processed_bucket" {
-  bucket = var.processed_bucket_name
-
-  tags = {
-    Name = var.processed_bucket_name
-  }
+# Output the source bucket name
+output "source_bucket_name" {
+  value = aws_s3_bucket.source_bucket.bucket
 }
 
-resource "aws_s3_bucket_versioning" "processed_versioning" {
-  bucket = aws_s3_bucket.processed_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
+# Output the destination bucket name
+output "destination_bucket_name" {
+  value = aws_s3_bucket.destination_bucket.bucket
 }
