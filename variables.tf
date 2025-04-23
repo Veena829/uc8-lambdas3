d25d72fed3ce9742daacd1bf86d9e4fbc49f0eab
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-23 13:11:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,16 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "us-west-2"
+variable "region" {
+  description = "The AWS region to deploy resources"
+  default     = "us-west-1"
 }
 
-variable "lambda_function_name" {
-  description = "The name of the Lambda function"
-  type        = string
-}
-
-variable "source_bucket_name" {
+variable "s3_bucket" {
   description = "The name of the source S3 bucket"
   type        = string
-   default     = "source-bucket"
+  default     = "source-bucket-8055"
 }
 
-variable "processed_bucket_name" {
-  description = "The name of the processed S3 bucket"
+variable "destination_bucket" {
+  description = "The name of the destination S3 bucket"
   type        = string
-default     = "processed-bucket"
+  default     = "destination-bucket-8055"
 }
