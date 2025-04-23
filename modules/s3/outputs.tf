3867e55b984ec0e34b5e74496ebb1367fd294fc5
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-23 13:09:11 +0530

    Update outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,8 +1,9 @@
-output "source_bucket" {
+# Output the source bucket name
+output "source_bucket_name" {
   value = aws_s3_bucket.source_bucket.bucket
 }
 
-output "processed_bucket" {
-  value = aws_s3_bucket.processed_bucket.bucket
+# Output the destination bucket name
+output "destination_bucket_name" {
+  value = aws_s3_bucket.destination_bucket.bucket
 }
-
