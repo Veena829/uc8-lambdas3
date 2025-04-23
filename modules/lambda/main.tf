5d879ff486ac0219c11c563f6e6a2903ba8c0e69
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-23 13:13:04 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -31,11 +31,4 @@ resource "aws_lambda_permission" "allow_s3" {
   source_arn    = "arn:aws:s3:::source-bucket-images1111121"
 }
 
-# filepath: /workspaces/AssistedProjects/use-case/UC-8-Lambda-S3/modules/lambda/outputs.tf
-output "lambda_function_arn" {
-  value = aws_lambda_function.image_processor.arn
-}
-# filepath: /workspaces/AssistedProjects/use-case/UC-8-Lambda-S3/modules/lambda/outputs.tf
-output "lambda_permission_id" {
-  value = aws_lambda_permission.allow_s3.id
-}
+
