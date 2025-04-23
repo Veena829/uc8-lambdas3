bb1fcfd158450042e56c7b44e70310981ceff7d5
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-23 13:08:59 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -31,12 +31,3 @@ resource "aws_s3_bucket_notification" "source_bucket_notification" {
   ]
 }
 
-# Output the source bucket name
-output "source_bucket_name" {
-  value = aws_s3_bucket.source_bucket.bucket
-}
-
-# Output the destination bucket name
-output "destination_bucket_name" {
-  value = aws_s3_bucket.destination_bucket.bucket
-}
